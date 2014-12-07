441bf3c67d6e54cccffe822fa6dc71a54a19bd8b
Andrey Nazarchuk <andynazay153@gmail.com>
2014-12-06 22:30:08 -0800

    fixes

--- ModPE/Mods/Kits/Kits.js
+++ ModPE/Mods/Kits/Kits.js
@@ -77,7 +77,7 @@ function updateVersion() {
                 var ru  = new java.lang.Runnable() {
                     run: function() {
                         try {
-                            var urls = new java.net.URL("https://raw.githubusercontent.com/AndreyNazarchuk/ModPE-Scripts-by-andynazay153/master/ModPE/Mods/Kits/KitsV6.js");
+                            var urls = new java.net.URL("https://raw.githubusercontent.com/AndreyNazarchuk/ModPE-Scripts-by-andynazay153/master/ModPE/Mods/Kits/Kits.js");
                             var check = urls.openConnection();
                             check.setRequestMethod("GET");
                             check.setDoOutput(true);
