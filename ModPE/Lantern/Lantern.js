0d1181dcce779b4939ba306585fc239bf123f690
Andrey Nazarchuk <andynazay153@gmail.com>
2015-02-08 11:57:39 -0800

    there

--- ModPE/Lantern/Lantern.js
+++ ModPE/Lantern/Lantern.js
@@ -12,9 +12,120 @@ You may do so in any reasonable manner, but not in any way that suggests the lic
 ShareAlike - If you remix, transform, or build upon the material, you must ask the Owner of this mod if you can release it to the public.
 No additional restrictions - You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
 */
+//Auto update 
+var version = "1.0"; 
+var checkForUpdate=false;
+var updateWindow=false; 
+var newUpdate;
+var updateMod;
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get(); 
 
+ function dip2px(ctx, dips){
+ return Math.ceil(dips * ctx.getResources().getDisplayMetrics().density);
+} 
 
+ function checkVersion() {
+    var r  = new java.lang.Runnable() {
+        run: function() {
+            try {
+                var urls= new java.net.URL("https://raw.githubusercontent.com/AndreyNazarchuk/ModPE-Scripts-by-andynazay153/master/ModPE/Lantern/lanternversion.txt");
+                var check = urls.openConnection();
+                check.setRequestMethod("GET");
+                check.setDoOutput(true);
+                check.connect();
+                check.getContentLength();
+                var script = check.getInputStream();
+                var typeb = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+                var byteCount = 0; 
+                var checkedVersion;
+                while((byteCount = script.read(typeb)) != -1) { 
+                    checkedVersion = new java.lang.String(typeb, 0, byteCount);               
+                }
+				newUpdate = checkedVersion;
+				if(version+"\n" != checkedVersion) {
+                    print("New version is available! " + newUpdate);
+                    updateWindow=true;
+                }
+                else if(version+"\n"==checkedVersion){
+                print("No updates available");
+                }
+            }
+            catch(err) {
+                print("Update check failed ");
+                if(err=="JavaException: java.net.UnknownHostException: raw.githubusercontent.com") {
+                                print("No internet connection.");
+                            }
+                            else {
+                                print("Error: \n" + err);
+                            } 
+            }
+        }
+    }
+    var threadt = new java.lang.Thread(r);
+    threadt.start();
+}
+function updateVersion() {
+    try {
+        var upd = new android.app.AlertDialog.Builder(ctx);
+        upd.setTitle("New version available!");
+        upd.setMessage("An update to Lantern was found!\nWould you like to update it?\nYour version: " + version + "\nNew version: " + newUpdate);
+        upd.setNegativeButton("Later", new android.content.DialogInterface.OnClickListener() {
+            onClick: function(par1) {
+            dialog.dismiss(); 
+   }
+        });
+        upd.setPositiveButton("Update", new android.content.DialogInterface.OnClickListener() {
+            onClick: function(par1) {
+                var ru  = new java.lang.Runnable() {
+                    run: function() {
+                        try {
+                            var urls = new java.net.URL("https://raw.githubusercontent.com/AndreyNazarchuk/ModPE-Scripts-by-andynazay153/master/ModPE/Lantern/Lantern.js");
+                            var check = urls.openConnection();
+                            check.setRequestMethod("GET");
+                            check.setDoOutput(true);
+                            check.connect();
+                            check.getContentLength();
+                            var script = check.getInputStream();
+                            var typeb = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+                            var byteCount = 0;
+                            while((byteCount = script.read(typeb)) != -1) { 
+                                updateMod += new java.lang.String(typeb, 0, byteCount);               
+                            }
+                            var modpeFolder = ctx.getDir("modscripts", 0);
+                            var modpeFile = new java.io.File(modpeFolder, "Lantern.js");
+                            var update = new java.io.PrintWriter(modpeFile);
+                            update.write(updateMod);
+                            update.flush();
+                            update.close();
+                            
+                            try {
+                                net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(modpeFile, false);
+                                net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(modpeFile, true);
+								print("Downloaded and enabled!");
+								  
+                            }
+                            catch(err) {
+                                print("Error: \n" + err);
+                            }
+                        }
+                        catch(err) {
+                            print("Error: \n" + err);
+                        }
+                    }
+                }
+                var threadt = new java.lang.Thread(ru);
+                threadt.start();
+            }
+        });
+        var dialog = upd.create();
+        dialog.show() 
+    }
+    catch(err) {
+        print("Error: \n" + err);
+    }
+}
 //Lantern Code
+
 Block.defineBlock(250, "Lantern",[["cactus", 2],["cactus", 2],["cake_bottom", 0],["cake_bottom", 0],["cake_bottom", 0],["cake_bottom", 0]], 2, false, 0);
 Block.setDestroyTime(250,1);
 Block.setShape(250, 1/4, 0, 1/4, 3/4, 3/4, 3/4);
@@ -29,9 +140,34 @@ Item.addShapedRecipe(250, 4, 0, [
 	
 function newLevel(){
 	Player.addItemCreativeInv(250, 1, 0);
- 
- function procCmd(cmd) {
- if(cmd == "debuglantern"){
+	if(checkForUpdate==false) {
+        print("Checking for updates");
+        ctx.runOnUiThread(new java.lang.Runnable({
+            run: function() {
+                try {
+                    checkVersion();
+                }
+                catch(err) {
+                    print("Error: \n"+err);
+                }
+            }
+        }));
+        checkForUpdate=true;
+    }
+    if(updateWindow) {
+        ctx.runOnUiThread(new java.lang.Runnable({
+            run: function() {
+                try {
+                    updateVersion();
+                }
+                catch(err) {
+                    print("Error: \n" + err);
+                }
+            }
+        }));
+        updateWindow=false;
+ function procCmd(command) {
+ if(command == "debuglantern"){
 Player.addItemInventory(339,10);
 Player.addItemInventory(50,10);
 }
@@ -42,5 +178,6 @@ function destroyBlock(x, y, z){
 	if(blockId == 250){
 	preventDefault();
 	Level.destroyBlock(x, y, z, true);
+}
 }
 }
