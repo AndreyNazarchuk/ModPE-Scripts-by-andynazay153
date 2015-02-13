64af8032fad154731c3d615a4b2bdf87b4ce8c8d
Andrey Nazarchuk <andynazay153@gmail.com>
2015-02-12 16:29:55 -0800

    Lots of changes in Useful mod

--- ModPE/Useful/UsefulMod.js
+++ ModPE/Useful/UsefulMod.js
@@ -1,16 +1,11 @@
 /*
-Made by andynazay153.
-This is my mod. Please do not copy or add it to anything or make a video without Asking me ;)
-This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
-You are free to:
-Share - copy and redistribute the material in any medium or format
-Adapt - remix, transform, and build upon the material for any purpose, even commercially, But with credit to the original author.
-The licensor cannot revoke these freedoms as long as you follow the license terms.
-Under the following terms:
-Attribution - You must give appropriate credit, provide a link to the license, and indicate if changes were made. 
-You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
-ShareAlike - If you remix, transform, or build upon the material, you must ask the Owner of this mod if you can release it to the public.
-No additional restrictions - You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+ * Useful mod v1.0
+ * © 2015 by andynazay153
+ * Auto-Update code used with permission from Bajan_Deadman0
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
 */
 
 //GUI Code
@@ -107,101 +102,116 @@ function mainMenu(){
             menuLayout.addView(button3);
 			*/
             
-            var button10 = new android.widget.Button(ctx);
-            button10.setText("Set time to Day");
-            button10.setOnClickListener(new android.view.View.OnClickListener({
-                onClick: function(viewarg){
-                    Level.setTime(1000);
-                }
-            }));
-            menuLayout.addView(button10);
-			
-			var button100 = new android.widget.Button(ctx);
-            button100.setText("Set time to Night");
-            button100.setOnClickListener(new android.view.View.OnClickListener({
-                onClick: function(viewarg){
-                    Level.setTime(14000);
-                }
-            }));
-            menuLayout.addView(button100);
-			
-			var button4 = new android.widget.Button(ctx);
-            button4.setText("Gamemode");
+            var button4 = new android.widget.Button(ctx);
+            button4.setText("Set time to Day");
             button4.setOnClickListener(new android.view.View.OnClickListener({
                 onClick: function(viewarg){
-                    
+                    Level.setTime(1000);
                 }
             }));
             menuLayout.addView(button4);
 			
 			var button5 = new android.widget.Button(ctx);
-            button5.setText("Flying On");
+            button5.setText("Set time to Night");
             button5.setOnClickListener(new android.view.View.OnClickListener({
                 onClick: function(viewarg){
-                    Player.setCanFly(true);
-                    Player.setFlying(true);
+                    Level.setTime(14000);
                 }
             }));
             menuLayout.addView(button5);
-            
-            var flyOff = new android.widget.Button(ctx);
-            flyOff.setText("Flying Off");
-            flyOff.setOnClickListener(new android.view.View.OnClickListener({
-                onClick: function(viewarg){
-                    Player.setCanFly(false);
-                    Player.setFlying(false);
-                }
-            }));
-            menuLayout.addView(flyOff);
 			
 			var button6 = new android.widget.Button(ctx);
-            button6.setText("Infinite Health ON");
+            button6.setText("Creative");
             button6.setOnClickListener(new android.view.View.OnClickListener({
                 onClick: function(viewarg){
-						Player.setHealth(99999999999);
+                    Level.setGameMode(1);
                 }
             }));
             menuLayout.addView(button6);
 			
-			var buttonInfHO = new android.widget.Button(ctx);
-            buttonInfHO.setText("Infinite Health OFF");
-            buttonInfHO.setOnClickListener(new android.view.View.OnClickListener({
-                onClick: function(viewarg){
-						Player.setHealth(20);
-                }
-            }));
-            menuLayout.addView(buttonInfHO);
-			
 			var button7 = new android.widget.Button(ctx);
-            button7.setText("Ride an entity");
+            button7.setText("Survival");
             button7.setOnClickListener(new android.view.View.OnClickListener({
                 onClick: function(viewarg){
-					preventDefault();
-					function attackHook(attacker, victim){
-					rideAnimal(attacker, victim);
-					clientMessage("Punch an animal to ride it");
-					}
+                    Level.setGameMode(0);
                 }
             }));
             menuLayout.addView(button7);
 			
 			var button8 = new android.widget.Button(ctx);
-            button8.setText("Teleport");
+            button8.setText("Flying On");
             button8.setOnClickListener(new android.view.View.OnClickListener({
                 onClick: function(viewarg){
-                    //Your Code
+                    Player.setCanFly(true);
+                    Player.setFlying(true);
                 }
             }));
             menuLayout.addView(button8);
-			
-			var button9 = new android.widget.Button(ctx);
-            button9.setText("Commit Suicide");
+            
+            var button9 = new android.widget.Button(ctx);
+            button9.setText("Flying Off");
             button9.setOnClickListener(new android.view.View.OnClickListener({
+                onClick: function(viewarg){
+                    Player.setCanFly(false);
+                    Player.setFlying(false);
+                }
+            }));
+            menuLayout.addView(button9);
+			
+			var button10 = new android.widget.Button(ctx);
+            button10.setText("Infinite Health ON");
+            button10.setOnClickListener(new android.view.View.OnClickListener({
+                onClick: function(viewarg){
+						Player.setHealth(99999999999);
+						clientMessage(ChatColor.RED + "WARNING:");
+						clientMessage(ChatColor.WHITE + "You will die and loose your items when you leave the game!");
+						clientMessage(ChatColor.GREEN + "Unless you turn off Infinite Health when you are done with it.");
+                }
+            }));
+            menuLayout.addView(button10);
+			
+			var button11 = new android.widget.Button(ctx);
+            button11.setText("Infinite Health OFF");
+            button11.setOnClickListener(new android.view.View.OnClickListener({
+                onClick: function(viewarg){
+						Player.setHealth(20);
+                }
+            }));
+            menuLayout.addView(button11);
+			
+			/*
+			var button12 = new android.widget.Button(ctx);
+            button12.setText("Ride an Animal");
+            button12.setOnClickListener(new android.view.View.OnClickListener({
+                onClick: function(viewarg){
+					clientMessage(ChatColor.GREEN + "Punch an animal to ride it");
+					Entity.rideAnimal(attacker, victim);
+					rideAnimal(attacker, victim);
+					preventDefault();
+                }
+            }));
+            menuLayout.addView(button12);
+			*/
+			
+			/*
+			var button13 = new android.widget.Button(ctx);
+            button13.setText("Teleport");
+            button13.setOnClickListener(new android.view.View.OnClickListener({
+                onClick: function(viewarg){
+                    
+                }
+            }));
+            menuLayout.addView(button13);
+			*/
+			
+			var button14 = new android.widget.Button(ctx);
+            button14.setText("Commit Suicide");
+            button14.setOnClickListener(new android.view.View.OnClickListener({
                 onClick: function(viewarg){
                     Player.setHealth(0);
                 }
             }));
-            menuLayout.addView(button9);
+            menuLayout.addView(button14);
 			
 			var closebutton1 = new android.widget.Button(ctx);
 			closebutton1.setText("Close Menu");
@@ -269,3 +279,143 @@ function leaveGame(){
     }}));
 }
 }
+
+//Auto-Update Code
+var version = "0.2"; 
+var checkForUpdate=false;
+var updateWindow=false; 
+var newUpdate;
+var updateMod;
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
+ function dip2px(ctx, dips){
+ return Math.ceil(dips * ctx.getResources().getDisplayMetrics().density);
+}
+
+ function checkVersion() {
+    var r  = new java.lang.Runnable() {
+        run: function() {
+            try {
+                var urls= new java.net.URL("");
+                var check = urls.openConnection();
+                check.setRequestMethod("GET");
+                check.setDoOutput(true);
+                check.connect();
+                check.getContentLength();
+                var script = check.getInputStream();
+                var typeb = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+                var byteCount = 0; 
+                var checkedVersion;
+                while((byteCount = script.read(typeb)) != -1) { 
+                    checkedVersion = new java.lang.String(typeb, 0, byteCount);               
+                }
+				newUpdate = checkedVersion;
+				if(version+"\n" != checkedVersion) {
+                    print("New version is available! " + newUpdate);
+                    updateWindow=true;
+                }
+                else if(version+"\n"==checkedVersion){
+                print("No updates available");
+                }
+            }
+            catch(err) {
+                print("Update check failed ");
+                if(err=="JavaException: java.net.UnknownHostException: raw.githubusercontent.com") {
+                                print("No internet connection.");
+                            }
+                            else {
+                                print("Error: \n" + err);
+                            } 
+            }
+        }
+    }
+    var threadt = new java.lang.Thread(r);
+    threadt.start();
+}
+function updateVersion() {
+    try {
+        var upd = new android.app.AlertDialog.Builder(ctx);
+        upd.setTitle("New version available!");
+        upd.setMessage("An update to Useful Mod was found!\nWould you like to update it?\nYour version: " + version + "\nNew version: " + newUpdate);
+        upd.setNegativeButton("Later", new android.content.DialogInterface.OnClickListener() {
+            onClick: function(par1) {
+            dialog.dismiss(); 
+   }
+        });
+        upd.setPositiveButton("Update", new android.content.DialogInterface.OnClickListener() {
+            onClick: function(par1) {
+                var ru  = new java.lang.Runnable() {
+                    run: function() {
+                        try {
+                            var urls = new java.net.URL("");
+                            var check = urls.openConnection();
+                            check.setRequestMethod("GET");
+                            check.setDoOutput(true);
+                            check.connect();
+                            check.getContentLength();
+                            var script = check.getInputStream();
+                            var typeb = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+                            var byteCount = 0;
+                            while((byteCount = script.read(typeb)) != -1) { 
+                                updateMod += new java.lang.String(typeb, 0, byteCount);               
+                            }
+                            var modpeFolder = ctx.getDir("modscripts", 0);
+                            var modpeFile = new java.io.File(modpeFolder, "UsefulMod.js");
+                            var update = new java.io.PrintWriter(modpeFile);
+                            update.write(updateMod);
+                            update.flush();
+                            update.close();
+                            
+                            try {
+                                net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(modpeFile, false);
+                                net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(modpeFile, true);
+								print("Downloaded and enabled!");
+								  
+                            }
+                            catch(err) {
+                                print("Error: \n" + err);
+                            }
+                        }
+                        catch(err) {
+                            print("Error: \n" + err);
+                        }
+                    }
+                }
+                var threadt = new java.lang.Thread(ru);
+                threadt.start();
+            }
+        });
+        var dialog = upd.create();
+        dialog.show() 
+    }
+    catch(err) {
+        print("Error: \n" + err);
+    }
+}
+	if(checkForUpdate==false) {
+        print("Checking for updates");
+        ctx.runOnUiThread(new java.lang.Runnable({
+            run: function() {
+                try {
+                    checkVersion();
+                }
+                catch(err) {
+                    print("Error: \n"+err);
+                }
+            }
+        }));
+        checkForUpdate=true;
+    }
+    if(updateWindow) {
+        ctx.runOnUiThread(new java.lang.Runnable({
+            run: function() {
+                try {
+                    updateVersion();
+                }
+                catch(err) {
+                    print("Error: \n" + err);
+                }
+            }
+        }));
+        updateWindow=false;
+	}
